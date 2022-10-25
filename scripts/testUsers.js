d408150d1951edae9ec3e50b1fabcbf20e78ddc8
Ahmad Mustafeen <engr.ahmadmustafeen@gmail.com>
2022-10-25 18:22:31 +0500

    attendance adding and fetching that attendance working.

--- scripts/testUsers.js
+++ scripts/testUsers.js
@@ -7,20 +7,49 @@
 const hre = require("hardhat");
 
 async function main() {
+  const [deployer, user1] = await hre.ethers.getSigners();
   const Users = await hre.ethers.getContractFactory("Main");
   const userContract = await Users.deploy();
 
   await userContract.deployed();
 
-  const allPositions = await userContract.FetchAllPositions();
-  console.log(allPositions)
 
-  await userContract.CreateUser("Ahmad","Mustafeen","NEWEST_POSITION","0x06Cf79f8a04fdcb2Cf75069eE8FFa3575d94EfdB");
-  await userContract.CreateUser("Bilal","Bilal","NEWEST_POSITION","0x665992de65856FECE65F0064Abf57241Ce831369");
-  await userContract.CreateUser("Bilal","Bilal","NEWEST_POSITION","0x06Cf79f8a04fdcb2Cf75069eE8FFa3575d94EfdB");
-  const allUsers = await userContract.mainFunction();
-  console.log(allUsers)
+
+  // **************** %%%%%%%%%%%%% ADDING USER CHECK  %%%%%%%%%%%%% **************** 
+
+
+  // const allPositions = await userContract.FetchAllPositions();
+  // console.log(allPositions)
+  // await userContract.CreateUser("Ahmad","Mustafeen","NEWEST_POSITION","0x06Cf79f8a04fdcb2Cf75069eE8FFa3575d94EfdB");
+  // await userContract.CreateUser("Bilal","Bilal","NEWEST_POSITION","0x665992de65856FECE65F0064Abf57241Ce831369");
+  // await userContract.CreateUser("Deployer","Deployer","NEWEST_POSITION","0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+  // const allUsers = await userContract.mainFunction();
+  // console.log({deployer:deployer.address})
+
+  // **************** %%%%%%%%%%%%% ADDING USER CHECK  %%%%%%%%%%%%% **************** 
+
+
+
+
+  // **************** %%%%%%%%%%%%% ADDING ATTENDANCE CHECK  %%%%%%%%%%%%% **************** 
+
+  // await userContract.markAttendance();
+  // await userContract.markAttendance();
+  // await userContract.markAttendance();
+
+  // const allAttendance = await userContract.fetchAttendance();
+  // console.log({allAttendance:allAttendance.map(item=>{return{...item,dateStamp: new Date(parseInt(Object.values(item.dateStamp)[0])).toDateString()}})})
+
+  // **************** %%%%%%%%%%%%% ADDING ATTENDANCE CHECK  %%%%%%%%%%%%% **************** 
+
+
+
+
   
+
+
+
+
 }
 main().catch((error) => {
   console.error(error);
